77a66df4de6d083ca96fde8f9f3b760a795802e0
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2023-03-07 22:11:14 -0600

    git init

---
+++ App.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+
+const app = express();
+const port = 3000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.get('/location', async (req, res) => {
+  try {
+    const location = req.query.location
+
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=1500&type=cafe&key=AIzaSyDzor3AfBqVGEEtLnHBCnwwGJ_4uRMHEJc`
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch directions from Google');
+    }
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
